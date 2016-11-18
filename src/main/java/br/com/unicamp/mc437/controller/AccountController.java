6e38ec133c6b9308e2be9f273e6dc252fae94bc2
Gustavo Pereira <gustavoppereira@gmail.com>
2016-11-18 21:50:29 -0200

    Adding entry point

--- src/main/java/br/com/unicamp/mc437/controller/AccountController.java
+++ src/main/java/br/com/unicamp/mc437/controller/AccountController.java
@@ -3,6 +3,9 @@ package br.com.unicamp.mc437.controller;
 import br.com.unicamp.mc437.model.Account;
 import br.com.unicamp.mc437.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,12 +30,11 @@ public class AccountController {
         return "Hello!";
     }
 
-    @RequestMapping(value = "/authenticate", produces={"application/json"}, method = RequestMethod.POST)
-    public Account authenticate(@RequestBody Account account, HttpServletRequest request,
-                                HttpServletResponse response) {
+    @RequestMapping(value = "/authenticate", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public ResponseEntity<Account> authenticate(@RequestBody Account account, HttpServletRequest request) {
         final Account user = accountService.accountByEmailAndPassword(account.getEmail(), account.getPassword());
 
         request.getSession(true);
-        return user;
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
